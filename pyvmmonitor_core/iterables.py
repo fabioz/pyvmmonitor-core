b66157f2ae74d80b43e563addded14386207219d
Fabio Zadrozny <fabiofz@gmail.com>
2016-06-30 10:33:18 -0300

    Added utility to count items an iterable (exhausting it in the process).

--- pyvmmonitor_core/iterables.py
+++ pyvmmonitor_core/iterables.py
@@ -5,3 +5,14 @@ def remove_duplicates(iterable, ret_type=list):
     seen = set()
     seen_add = seen.add
     return ret_type(x for x in iterable if not (x in seen or seen_add(x)))
+
+
+def count_iterable(iterable):
+    '''
+    Count the number of items in an iterable (note that this will exhaust it in the process and
+    it'll be unusable afterwards).
+    '''
+    i = 0
+    for _x in iterable:
+        i += 1
+    return i
