8b6660444efa99ccfceea710b0a6ff4efbb3a2ad
Fabio Zadrozny <fabiofz@gmail.com>
2016-10-26 14:37:14 -0200

    Created utility to print the time on some method.

---
+++ pyvmmonitor_core/time_utils.py
@@ -0,0 +1,24 @@
+def time_method(func):
+    '''
+    Helper decorator to time a function.
+
+    To use:
+
+    @time_method
+    def func():
+        ...
+
+    '''
+    import time
+
+    try:
+        func_name = func.func_name
+    except:
+        func_name = func.__name__
+
+    def wrap(*args, **kwargs):
+        time1 = time.time()
+        ret = func(*args, **kwargs)
+        print('%s function took %0.3f s' % (func_name, (time.time() - time1)))
+        return ret
+    return wrap
