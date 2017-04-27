3206ec1939843ae8a7bad703d9e160d01508494e
Fabio Zadrozny <fabiofz@gmail.com>
2017-04-27 08:13:21 -0300

    Utility for debugging (magic_vars) .

---
+++ pyvmmonitor_core/magic_vars.py
@@ -0,0 +1,31 @@
+'''
+Helpers for debugging:
+
+To use:
+
+set_magic_var()
+
+...
+
+Somewhere else in the code:
+
+if is_magic_var_set():
+    import pydevd;pydevd.settrace()
+
+License: LGPL
+Copyright: Brainwy Software
+'''
+
+_magic_vars = {}
+
+
+def set_magic_var(var_name='global_var', value=1):
+    _magic_vars[var_name] = value
+
+
+def is_magic_var_set(var_name='global_var'):
+    return _magic_vars.get(var_name) is not None
+
+
+def get_magic_var(var_name='global_var'):
+    return _magic_vars.get(var_name)
