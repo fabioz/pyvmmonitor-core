ff867f4ecb23307c31355e4433b9a14150822d0e
Fabio Zadrozny <fabiofz@gmail.com>
2024-08-10 08:15:50 -0300

    Fixing some deprecation warnings.

--- pyvmmonitor_core/thread_utils.py
+++ pyvmmonitor_core/thread_utils.py
@@ -7,4 +7,4 @@ Copyright: Brainwy Software
 
 def is_in_main_thread():
     import threading
-    return threading.currentThread().name == 'MainThread'
+    return threading.current_thread().name == 'MainThread'
